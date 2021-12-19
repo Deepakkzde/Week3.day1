9b5f89fa55d55e58b8eedaccab202c55d5f2a7c9
Deepakkzde <95161019+Deepakkzde@users.noreply.github.com>
2021-12-19 12:17:25 +0530

    Add files via upload

---
+++ Assignments6.java
@@ -0,0 +1,23 @@
+package Week3.day1;
+
+public class Assignments6 {
+
+	public static void main(String[] args) {
+		
+				
+				String str3 = new String("Kutty");
+				String str4 = new String("Kutty");
+				 
+				// == should be used during refence comparision...
+				// whereas equals should be used to compare the content of the 2 Strings
+				
+				if(str3==str4) {
+					 System.out.println(" Same text");
+				 }
+				 else
+					 System.out.println("Diff text");
+				 
+				 System.out.println("***");
+			}
+
+		}
