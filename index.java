8459f56e4299b7f471f9f8d44686905f39377ed9
Deepakkzde <95161019+Deepakkzde@users.noreply.github.com>
2021-12-19 12:23:33 +0530

    Add files via upload

---
+++ index.java
@@ -0,0 +1,15 @@
+package Week3.day1;
+
+public class index {
+
+	public static void main(String[] args) {
+
+		String text = "Java Exercise";
+		
+		char e1 = text.charAt(5);
+		System.out.println(" fifth Index value " +e1);
+		
+		char e2 = text.charAt(11);
+		System.out.println(" 11th Index value " +e2);
+		
+	}}
