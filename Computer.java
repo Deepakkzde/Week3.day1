d7cdac0875efe2cc2fc681988f3f24a9922bccfa
Deepakkzde <95161019+Deepakkzde@users.noreply.github.com>
2021-12-19 11:55:28 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,15 @@
+package Week3.day1;
+
+public class Computer {
+
+		
+		public void computerModel() {
+			System.out.println("Dell V520");
+
+		
+		}
+		}
+
+
+	
+
---
+++ Desktop.java
@@ -0,0 +1,15 @@
+package Week3.day1;
+
+public class Desktop extends Computer{
+	
+	public void desktopSize() {
+		System.out.println("14 Inches");
+
+	}
+	public static void main(String[] args) {
+		Computer dell = new Computer();
+		dell.computerModel();
+		
+	}
+
+}
